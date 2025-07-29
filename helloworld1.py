9039b8f8d4ece4a69924614f116002769c32e0dd
ulrich-droid <ulricharegba@gmail.com>
2025-07-29 20:14:05 +0000

    First commit

---
+++ helloworld1.py
@@ -0,0 +1,16 @@
+  #Days 1_30DaysOfpython challenge
+print(2+3)     #addition(+)
+print(3-2)     #soustraction(-)
+print(2 * 3)   #multiplication(*)
+print(2 / 3)   #division(/)
+print(3 ** 2)  #exponential(**)
+print(3 % 2)   #modulus(%)
+print(3 // 2)  #Floor division operator(//)
+
+print(type(10))   #int
+print(type(3.14)) #float
+print(type(1+3j)) #complexe numero imprime
+print(type('asabeneh')) #string
+print(type([1,2,3])) #list
+print(type({'name' : 'asabeneh'})) #dictionary imprimer
+print(type((9.8,3.14,2.7))) #tuple
