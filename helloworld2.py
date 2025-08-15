cf40bbea0d500122d950fb16d6b81c2d4b578d39
ulrich-droid <ulricharegba@gmail.com>
2025-08-15 23:09:52 +0000

    first commit

---
+++ helloworld2.py
@@ -0,0 +1,28 @@
+print("hello, world!")
+print(type(4.9))
+print(min([20, 30, 40, 50]))
+print(len("hello, world!"))
+print(max(20, 30, 40, 50))
+print(sum([20, 30, 40, 50]))
+print(type("hello, world!"))
+print(str(10))
+print(int("10"))
+print(float(10))
+input("Enter your name:")
+
+
+
+#int to float
+num_int = 10
+print("num_int" ,num_int)
+num_float = float(num_int)
+print("num_float:" , num_float)
+
+Gravity = 9.81
+print(int(Gravity))
+
+#str to list
+first_name = "asabeneh"
+print(first_name)
+first_name_to_liste = list(first_name)
+print(first_name_to_liste)
